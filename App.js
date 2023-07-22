7b8585a8360e3b22e85e2056726b6ae4f3b192d5
phitsanu17 <ia.phitsanu_st@tni.ac.th>
2023-07-22 11:17:36 +0700

    "NewRepositories"

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Cat from './components/Cat';
+import ViewBoxesWithColorAndText from './components/ViewBoxesWithColorAndText';
+import DisplayandImage from './components/DisplayandImage';
+import LotsOfGreeting from './components/LotsOfGreeting';
+import CustomText from './components/CustomText';
+import Form from './components/From';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      
+      {/* <ViewBoxesWithColorAndText/>  */}
+      {/* <DisplayandImage/> */}
+      {/* <LotsOfGreeting/> */}
+      {/* <CustomText/> */}
+      <Form/>
+
     </View>
   );
 }
